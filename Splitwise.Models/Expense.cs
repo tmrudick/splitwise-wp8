bae1ec408f84284ff821e6047aa5f79c1017d88e
Tom Rudick <tmrudick@gmail.com>
2013-01-11 17:49:55 -0800

    Creating models project

---
+++ Splitwise.Models/Expense.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Splitwise.Models
+{
+    public class Expense
+    {
+        public long ExpenseId { get; set; }
+    }
+}
---
+++ Splitwise.Models/Friend.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Splitwise.Models
+{
+    public class Friend
+    {
+        public long FriendId { get; set; }
+    }
+}
