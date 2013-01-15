1329ef99996fcac560070ba5012da1b5f7453c7c
Tom Rudick <tmrudick@gmail.com>
2013-01-14 23:27:24 -0800

    Creating Home view and view model

---
+++ splitwise-wp8/ViewModels/HomeViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Splitwise.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Splitwise.ViewModels
+{
+    public class HomeViewModel
+    {
+        private ObservableCollection<User> _friends = new ObservableCollection<User>();
+        private SplitwiseProxy proxy = SplitwiseProxy.GetProxyInstance();
+
+        public HomeViewModel()
+        {
+            LoadFriends();
+        }
+
+        private async void LoadFriends()
+        {
+            IEnumerable<User> friends = await proxy.GetFriends();
+
+            foreach (var friend in friends)
+            {
+                _friends.Add(friend);
+            }
+        }
+
+        public ObservableCollection<User> Friends
+        {
+            get
+            {
+                return _friends;
+            }
+        }
+    }
+}
