80cdf6ccd95141203a8d6fd7b91eefb8efc32ac9
Tom Rudick <tmrudick@gmail.com>
2013-01-14 23:48:14 -0800

    Adding friends list to Home view
    Just starting to bind data from the view model to the Home view.
    Starting with friends (because it is easy) and then will move onto
    expenses

--- splitwise-wp8/Views/Home.xaml.cs
+++ splitwise-wp8/Views/Home.xaml.cs
@@ -7,14 +7,20 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using Splitwise.ViewModels;
 
 namespace Splitwise.Views
 {
     public partial class Home : PhoneApplicationPage
     {
+        private HomeViewModel ViewModel;
+
         public Home()
         {
             InitializeComponent();
+
+            this.ViewModel = new HomeViewModel();
+            this.DataContext = this.ViewModel;
         }
     }
 }
