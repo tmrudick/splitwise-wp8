824fe56158f2110adfef7f143e3e25ef37eea685
Tom Rudick <tmrudick@gmail.com>
2013-02-06 21:37:15 -0800

    Implementing create expense proxy method

---
+++ splitwise-wp8/Utility/JsonHelpers.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Splitwise.Utility
+{
+    public static class JsonHelpers
+    {
+        public static bool AddIfNotDefault<T>(this JObject obj, string property, T value) {
+            if (EqualityComparer<T>.Default.Equals(value, default(T))) {
+                return false;
+            }
+
+            obj[property] = JToken.FromObject(value);
+
+            return true;
+        }
+    }
+}
