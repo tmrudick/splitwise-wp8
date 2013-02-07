fab3d72f284b35431095448c08dbd876cbdbf14f
Tom Rudick <tmrudick@gmail.com>
2013-02-06 23:00:46 -0800

    Making some things look somewhat nicer
    Upping the font and margins on the home view. Upping the font size on
    the friend view.

---
+++ splitwise-wp8/Converters/DebtColorConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Splitwise.Converters
+{
+    public class DebtColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            decimal amount = decimal.Parse(value.ToString());
+
+            SolidColorBrush brush = new SolidColorBrush();
+
+            if (amount < 0)
+            {
+                brush.Color = Colors.Red;
+            }
+            else if (amount > 0)
+            {
+                brush.Color = Colors.Green;
+            }
+            else
+            {
+                brush.Color = Colors.White;
+            }
+
+            return brush;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
